0fec515389ce6948d5d82d985fdcf8d97f47f937
Gennaro Maida <gtm1235@gmail.com>
2023-03-28 11:51:32 -0500

    added 5 and 15 minute loop

--- main.py
+++ main.py
@@ -35,22 +35,24 @@ warnings.simplefilter('ignore', category=RuntimeWarning)
 
 
 # set number of minutes for sliding window in calculation for SI, HF/LF, VLF, Poincare
-NUM_MINUTES_WINDOW = 5
+NUM_MINUTES_WINDOW = [5, 15]
 
 si_values = []
 time_list = []
 lf_hf_list = []
-vlf_list = []
-total_power_list = []
-si_sleep = []
-sd_list = []
 date_time_list = []
+# vlf_list = []
+# total_power_list = []
+# si_sleep = []
+# sd_list = []
+
 
 csv_files = get_csv_files()
-# print(csv_files)
+#print(csv_files)
 
 # extarct information from filename
 name, first_date, last_date, files_sorted = extract_name_date(csv_files)
+
 
 # file for range of dates for daily metrics
 filename_daily = f'{name}_{first_date}_to_{last_date}_daily.csv'
@@ -59,72 +61,96 @@ check_file_exists(filename_daily)
 filename_5min_window = f'{name}_{first_date}_to_{last_date}_5min.csv'
 check_file_exists(filename_5min_window)
 
-for file in files_sorted:
-    vlf_list = []
-    total_power_list = []
-    si_sleep = []
-    sd_list = [[], [], []]
+filename_15min_window = f'{name}_{first_date}_to_{last_date}_15min.csv'
+check_file_exists(filename_15min_window)
 
+#print(files_sorted)
+for file in files_sorted:
     current_date = file[2]
     current_date = current_date[:4] + '-' + \
-        current_date[4:6] + '-' + current_date[6:]
+    current_date[4:6] + '-' + current_date[6:]
+    #print('.'.join(file))
     file = '.'.join(file)
-    data = pd.read_csv(file)
 
-    df_hrv = find_peaks_and_scale(data)
+    #print(current_date)
+    for minutes_window in NUM_MINUTES_WINDOW:
+        vlf_list = []
+        total_power_list = []
+        si_sleep = []
+        sd_list = [[], [], []]
+        si_values = []
+        time_list = []
+        lf_hf_list = []
+        date_time_list = []
 
-    hrv_range = create_time_intervals(NUM_MINUTES_WINDOW, df_hrv)
 
-    for n in range(len(hrv_range)-1):
-        # Caluclates values over window range for short term recovery
-        si_df = df_hrv.loc[df_hrv['ts'] < hrv_range[n+1]]
-        # print(len(si_df), "---")
-        si_df = si_df.loc[si_df['ts'] > hrv_range[n]]
-        # print(len(si_df), "+++")
-        si_np = si_df['hrv'].to_numpy()
-        si_np = si_np*1000
+        
+        data = pd.read_csv(file)
 
-        si_df_interpolated_nn_intervals = process_rr_nn_intervals(si_np)
+        df_hrv = find_peaks_and_scale(data)
 
-        if si_df_interpolated_nn_intervals == 0:
-            # si_values.append(0)
-            # time_list.append(hrv_range[n+1])
-            # lf_hf_list.append(0)
-            # date_time_list.append(convert_posix_time_to_date(hrv_range[n+1]))
+        hrv_range = create_time_intervals(minutes_window, df_hrv)
+        print(minutes_window, (hrv_range[0]-hrv_range[1])/60000)
 
-            continue
+        for n in range(len(hrv_range)-1):
+            # Caluclates values over window range for short term recovery
+            si_df = df_hrv.loc[df_hrv['ts'] < hrv_range[n+1]]
+            # print(len(si_df), "---")
+            si_df = si_df.loc[si_df['ts'] > hrv_range[n]]
+            # print(len(si_df), "+++")
+            si_np = si_df['hrv'].to_numpy()
+            si_np = si_np*1000
 
-        si = calculate_si(si_df_interpolated_nn_intervals)
-        # print(time_list)
-        try:
-            freq_measures = get_frequency_domain_features(
-                si_df_interpolated_nn_intervals, method='lomb')
-            # print('Y', freq_measures['lf_hf_ratio'])
-        except:
-            lf_hf_list.append(0)
+            si_df_interpolated_nn_intervals = process_rr_nn_intervals(si_np)
 
-        si_values.append(si)
-        time_list.append(hrv_range[n+1])
-        lf_hf_list.append(freq_measures['lf_hf_ratio'])
-        date_time_list.append(convert_posix_time_to_date(hrv_range[n+1]))
+            if si_df_interpolated_nn_intervals == 0:
+                # si_values.append(0)
+                # time_list.append(hrv_range[n+1])
+                # lf_hf_list.append(0)
+                # date_time_list.append(convert_posix_time_to_date(hrv_range[n+1]))
 
-    si_values_np = np.array(si_values)
-    si_diff_np = np.diff(si_values_np)
-    time_values_np = np.array(time_list)
-    time_diff_np = np.diff(time_values_np)
-    si_diff = (si_diff_np/time_diff_np)*1000*60
-    si_diff = np.insert(si_diff, 0, 0)
+                continue
 
-    # print(len(si_values), len(lf_hf_list), len(si_diff), len(time_list))
-    metrics_dict = {'si_values': si_values, 'lf_hf_list': lf_hf_list,
-                    'si_diff_per_min': si_diff, 'timestamp': time_list, 'datetime': date_time_list}
-    try:
-        # If the DataFrame exists, append the dictionary to it
-        metrics_5min_df = pd.concat(
-            [metrics_5min_df, pd.DataFrame.from_dict(metrics_dict)])
-    except NameError:
-        # If the DataFrame doesn't exist, create it and assign it the dictionary
-        metrics_5min_df = pd.DataFrame.from_dict(metrics_dict)
+            si = calculate_si(si_df_interpolated_nn_intervals)
+            # print(time_list)
+            try:
+                freq_measures = get_frequency_domain_features(
+                    si_df_interpolated_nn_intervals, method='lomb')
+                # print('Y', freq_measures['lf_hf_ratio'])
+            except:
+                lf_hf_list.append(0)
+
+            si_values.append(si)
+            time_list.append(hrv_range[n+1])
+            lf_hf_list.append(freq_measures['lf_hf_ratio'])
+            date_time_list.append(convert_posix_time_to_date(hrv_range[n+1]))
+
+        si_values_np = np.array(si_values)
+        si_diff_np = np.diff(si_values_np)
+        time_values_np = np.array(time_list)
+        time_diff_np = np.diff(time_values_np)
+        si_diff = (si_diff_np/time_diff_np)*1000*60
+        si_diff = np.insert(si_diff, 0, 0)
+
+        # print(len(si_values), len(lf_hf_list), len(si_diff), len(time_list))
+        metrics_dict = {'si_values': si_values, 'lf_hf_list': lf_hf_list,
+                        'si_diff_per_min': si_diff, 'timestamp': time_list, 'datetime': date_time_list}
+        if minutes_window == 5:
+            try:
+                # If the DataFrame exists, append the dictionary to it
+                metrics_5min_df = pd.concat(
+                    [metrics_5min_df, pd.DataFrame.from_dict(metrics_dict)])
+            except NameError:
+                # If the DataFrame doesn't exist, create it and assign it the dictionary
+                metrics_5min_df = pd.DataFrame.from_dict(metrics_dict)
+        elif minutes_window == 15:
+            try:
+                # If the DataFrame exists, append the dictionary to it
+                metrics_15min_df = pd.concat(
+                    [metrics_15min_df, pd.DataFrame.from_dict(metrics_dict)])
+            except NameError:
+                # If the DataFrame doesn't exist, create it and assign it the dictionary
+                metrics_15min_df = pd.DataFrame.from_dict(metrics_dict)
 
     # Calculate overnight values to show slow recovery
     si_np = df_hrv['hrv'].to_numpy()
@@ -192,4 +218,5 @@ for file in files_sorted:
 
 metrics_daily_df.to_csv(filename_daily, index=False)
 metrics_5min_df.to_csv(filename_5min_window, index=False)
+metrics_15min_df.to_csv(filename_15min_window, index=False)
 
